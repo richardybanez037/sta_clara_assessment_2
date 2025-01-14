b8408e40644d2ed0c6ebda4a6f65a218e18e28b0
richardybanez037 <richardybanez037@gmail.com>
2025-01-14 10:40:18 +0800

    temp fix

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,7 @@
 'use server'
 export const getSession = async() => {}
 export const login = async() => {}
-export const register = async(formData: FormData) => {}
+export const register = async(formData: FormData) => {
+  console.log(formData)
+}
 export const logout = async() => {}
